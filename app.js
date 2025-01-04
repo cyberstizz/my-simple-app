a3da60c18cf2b3054e56e11690d3123c63bb270f
Charles Lamb <charleslamb@charless-mbp.mynetworksettings.com>
2025-01-03 23:08:02 -0500

    changed the app.js to include navigation

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-import { ExpoRouter } from 'expo-router';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Import screens
+import InitialScreen from './src/screens/InitialScreen';
+import LoadingScreen from './src/screens/LoadingScreen';
+import ResultsScreen from './src/screens/ResultsScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <ExpoRouter />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Initial">
+        <Stack.Screen name="Initial" component={InitialScreen} />
+        <Stack.Screen name="Loading" component={LoadingScreen} />
+        <Stack.Screen name="Results" component={ResultsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
